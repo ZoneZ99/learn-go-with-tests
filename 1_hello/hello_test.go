d71e9958f289a23bc2d8d0757a98538d094af36d
Farhan Azyumardhi Azmi <sonofkyrat99@gmail.com>
2020-07-19 00:53:15 +0700

    Rename folders

---
+++ 1_hello/hello_test.go
@@ -0,0 +1,37 @@
+package main
+
+import "testing"
+
+func TestHello(test *testing.T) {
+
+	assertCorrectMessage := func(test *testing.T, got, want string) {
+		test.Helper()
+		if got != want {
+			test.Errorf("got %q want %q", got, want)
+		}
+	}
+
+	test.Run("saying hello to people", func(test *testing.T) {
+		got := Hello("Chris", "")
+		want := "Hello, Chris"
+		assertCorrectMessage(test, got, want)
+	})
+
+	test.Run("say 'Hello, World' when an empty string is supplied", func(test *testing.T) {
+		got := Hello("", "")
+		want := "Hello, World"
+		assertCorrectMessage(test, got, want)
+	})
+
+	test.Run("in Spanish", func(test *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(test, got, want)
+	})
+
+	test.Run("in French", func(test *testing.T) {
+		got := Hello("Paul", "French")
+		want := "Bonjour, Paul"
+		assertCorrectMessage(test, got, want)
+	})
+} 
